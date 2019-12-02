b99fa96984c7c173dcb7a6a7003cf01f7f558298
Sivarogu <clem.depraz@gmail.com>
2019-12-02 16:43:34 +0100

    Factorize method from 1362 characters to 355 characters by: - replacing map + collect with fold when necessary - using a bytes vec instead of char vec - removing other unnecessary intermediate casts - renaming variables by single letters - returning without return keyword and intermediate variables - removing comments and white spaces
    by Clément Depraz

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,6 @@
 use std::str;
- 
-const UPPERCASE_OFFSET: i8 = -65;
-const LOWERCASE_OFFSET: i8 = 26 - 97;
-const NUM_OFFSET: i8 = 52 - 48;
 
-fn base64decode(input: String) -> String {
-    let result = input.trim().chars()
-        .filter(|&ch| ch != '=')                                //Filter '=' chars
-        .map(|ch| {                                             //Map char values using Base64 Characters Table
-            let ascii = ch as i8;                           
-            let convert = match ch {
-                '0' ..= '9' => ascii + NUM_OFFSET,
-                'a' ..= 'z' => ascii + LOWERCASE_OFFSET,
-                'A' ..= 'Z' => ascii + UPPERCASE_OFFSET,
-                '+' => 62,
-                '/' => 63,
-                _ => panic!("Not a valid base64 encoded string")
-            };
-            format!("{:#08b}", convert)[2..].to_string()        //convert indices to binary format and remove the two first digits
-        })
-        .collect::<String>()                                    //concatenate the resulting binary values
-        .chars()
-        .collect::<Vec<char>>()
-        .chunks(8)                                              //split into 8 character chunks
-        .map(|chunk| {
-            let num_str = chunk.iter().collect::<String>();
-            usize::from_str_radix(&num_str, 2).unwrap() as u8   //convert the binary string into its u8 value
-        })
-        .collect::<Vec<_>>();
- 
-    let result = str::from_utf8(&result).unwrap();              //convert into UTF-8 string
-    return String::from(result);
-}
+fn base64decode(i:String)->String{String::from_utf8(i.trim().chars().filter(|&c|c!='=').fold(Vec::new(),|mut a,c|{a.extend(format!("{:06b}",match c{'0'..='9'=>c as i8+4,'a'..='z'=>c as i8-71,'A'..='Z'=>c as i8-65,'+'=>62,'/'=>63,_=>panic!()}).bytes());a}).chunks(8).map(|c|{u8::from_str_radix(str::from_utf8(&c).unwrap(),2).unwrap()}).collect()).unwrap()}
  
 fn main() {
     let mut input = String::new();
