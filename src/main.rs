ec94a287f494c413b4f8a17d4d8fdd3ce65799ee
Sivarogu <clem.depraz@gmail.com>
2019-12-31 16:02:51 +0100

    Finally reduce all code to 441 chars
    However the executable is still pretty heavy
    with 293KB using cargo build --release
    Of course, reducing code size is more of a challenge than usefull
    and in that cas doesn't improve performance or really reduce executable size

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,1 @@
-use std::str;
-
-fn base64decode(i:String)->String{String::from_utf8(i.trim().chars().filter(|&c|c!='=').fold(Vec::new(),|mut a,c|{a.extend(format!("{:06b}","ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".find(c).unwrap()).bytes());a}).chunks(8).map(|c|{u8::from_str_radix(str::from_utf8(c).unwrap(),2).unwrap()}).collect()).unwrap()}
- 
-fn main() {
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    println!("Input: {}", input);
- 
-    let output = base64decode(input);
-    println!("Output: {}", output);
-}
+use std::str;fn main(){let mut i=String::new();std::io::stdin().read_line(&mut i).unwrap();println!("Input: {}",i);println!("Output: {}",String::from_utf8(i.trim().chars().filter(|&c|c!='=').fold(Vec::new(),|mut a,c|{a.extend(format!("{:06b}","ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".find(c).unwrap()).bytes());a}).chunks(8).map(|c|{u8::from_str_radix(str::from_utf8(c).unwrap(),2).unwrap()}).collect()).unwrap());}
