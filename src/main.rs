1e727c7d7742a1ac0b741a71933f558152c8b9cd
Sivarogu <clem.depraz@gmail.com>
2019-12-31 15:46:31 +0100

    Factorize method from 355 to 336 characters by using a str.find() instead of a match block
    Without success or reducing nb of chars, I tried to:
    - rename methods like unwrap to a single letter variable
    - create a str using range (like in match)
    - not use chunks() method since it needs a slice
    - use a bits slice instead of bytes representing bits
    - cast less variables in general

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::str;
 
-fn base64decode(i:String)->String{String::from_utf8(i.trim().chars().filter(|&c|c!='=').fold(Vec::new(),|mut a,c|{a.extend(format!("{:06b}",match c{'0'..='9'=>c as i8+4,'a'..='z'=>c as i8-71,'A'..='Z'=>c as i8-65,'+'=>62,'/'=>63,_=>panic!()}).bytes());a}).chunks(8).map(|c|{u8::from_str_radix(str::from_utf8(&c).unwrap(),2).unwrap()}).collect()).unwrap()}
+fn base64decode(i:String)->String{String::from_utf8(i.trim().chars().filter(|&c|c!='=').fold(Vec::new(),|mut a,c|{a.extend(format!("{:06b}","ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".find(c).unwrap()).bytes());a}).chunks(8).map(|c|{u8::from_str_radix(str::from_utf8(c).unwrap(),2).unwrap()}).collect()).unwrap()}
  
 fn main() {
     let mut input = String::new();
